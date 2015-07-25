95ec0a7a6ba704f9cc77d5121f328ad18b9826a8
Jack <guitarpoet@gmail.com>
2015-07-25 16:43:12 +0800

    Added the json and jsonp support for ajax.

--- src/core/04_ajax.jsx
+++ src/core/04_ajax.jsx
@@ -29,12 +29,12 @@ class Ajax {
 		this.xhr = this.getXhr();
 	}
 
-	get(url, data, headers) {
-		return this.exec('GET', url, data, headers);
+	get(url, data, headers, type) {
+		return this.exec('GET', url, data, headers, type);
 	}
 
-	post(url, data, headers) {
-		return this.exec('POST', url, data, headers);
+	post(url, data, headers, type) {
+		return this.exec('POST', url, data, headers, type);
 	}
 
 	put(url, data, headers) {
@@ -49,7 +49,7 @@ class Ajax {
 		return this.exec('HEAD', url, data, headers);
 	}
 
-	exec(method, url, data, headers) {
+	exec(method, url, data, headers, type) {
 		return new Promise((resolve, reject) => {
 			let payload;
 			let data = data || {};
@@ -60,6 +60,44 @@ class Ajax {
 			if (method === 'GET' && payload) {
 				url += '?' + payload;
 				payload = null;
+			}
+
+			if(type == 'jsonp' && !lilium.inNode() && typeof document !== 'undefined') {
+
+				// We're in the browser, and trying to getting the data through jsonp
+				if(!Date.now){
+					Date.now = function() { return new Date().getTime(); };
+				}
+
+				//Use timestamp + a random factor to account for a lot of requests in a short time
+				//e.g. jsonp1394571775161 
+				var timestamp = Date.now();
+				var generatedFunction = 'jsonp'+Math.round(timestamp+Math.random()*1000001)
+
+				//Generate the temp JSONP function using the name above
+				//First, call the function the user defined in the callback param [callback(json)]
+				//Then delete the generated function from the window [delete window[generatedFunction]]
+				window[generatedFunction] = function(json){
+					resolve(json);
+					// IE8 throws an exception when you try to delete a property on window
+					// http://stackoverflow.com/a/1824228/751089
+					try {
+						delete window[generatedFunction];
+					} catch(e) {
+						window[generatedFunction] = undefined;
+					}
+				};
+
+				let script = document.createElement('script');
+				if(url.indexOf('?') != -1) {
+					url += '&callback=' + generatedFunction;
+				}
+				else {
+					url += '?callback=' + generatedFunction;
+				}
+				script.setAttribute("src", url);
+				document.getElementsByTagName("head")[0].appendChild(script)
+				return;
 			}
 
 			// open xhr
@@ -96,7 +134,11 @@ class Ajax {
 						reject(this.xhr.status, this.xhr.responseText, xhr);
 					}
 					else {
-						resolve(this.xhr.responseText, xhr);
+						let result = this.xhr.responseText;
+						if(type == 'json' || type == 'jsonp') {
+							result = JSON.parse(result);
+						}
+						resolve(result, xhr);
 					}
 				}
 			};
--- tests/core/ajax_spec.jsx
+++ tests/core/ajax_spec.jsx
@@ -1,4 +1,5 @@
 var core = require('../../core.js');
+var sleep = require('sleep');
 
 describe("core.ajax.suite", function() {
 	it("xhr.test", function() {
@@ -7,12 +8,21 @@ describe("core.ajax.suite", function() {
 		expect(typeof a.xhr).toBe('object');
 		a.timeout = 2000;
 		a.exec('GET','http://localhost').then((data) => {
-			console.info('A');
 			console.log(data);
 		}).catch((e) => {
 			expect(e).toEqual('TIMEOUT');
 			jasmine.done();
 		});
 		clock.tick(10000);
+	});
+
+	it("json.test", function() {
+		let a = new lilium.core.Ajax();
+		a.get('http://localhost/~jack/a.json', null, null, 'json').then((data) => {
+			console.dir(data);
+		}).catch((e) => {
+			console.dir(e);
+		});
+		sleep.sleep(2);
 	});
 });
